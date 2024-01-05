30f2b802aaa95a3e74da08883e6fe05057e376f6
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-01-05 22:44:31 +0100

    Added exit button behavior

---
+++ scenes/menu/Exit.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(self._button_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _button_pressed():
+	get_tree().quit()
+
