2270700a2772a9d43a4d737d3de69fb1fda95485
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-01-05 22:43:54 +0100

    Added main menu

---
+++ scenes/menu/MainMenu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/Play.grab_focus()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
