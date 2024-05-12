070039d26d04dc735bcd61ce1b30b1c48c0a86e5
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2024-05-12 12:40:51 +0200

    Added room type constants

--- Global.gd
+++ Global.gd
@@ -1,4 +1,14 @@
 extends Node
 
+const ROOM_TYPE_HALLWAY = 0
+const ROOM_TYPE_INNER_HALLWAY = 1
+const ROOM_TYPE_STANDARD_CLASSROOM = 2
+const ROOM_TYPE_LECTURE_HALL = 3
+const ROOM_TYPE_OFFICE = 4
+const ROOM_TYPE_BATHROOM = 5
+
 var main_scene
+
+var current_room
+
 var player
