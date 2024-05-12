b64d65bfbaa5100ff8538e74c88f9de9733c4a46
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2024-05-12 12:41:17 +0200

    Added main menu visibility

--- main.gd
+++ main.gd
@@ -4,6 +4,8 @@ extends Node2D
 func _ready():
 	Global.main_scene = self
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$UI/MainMenu.visible = true
+	$UI/GUI.visible = false
 
 
 func _process(delta):
--- ui/main_menu/play_button.gd
+++ ui/main_menu/play_button.gd
@@ -2,3 +2,7 @@ extends Button
 
 func _ready():
 	self.grab_focus()
+
+func _on_pressed():
+	# change scene to character select
+	pass
