fca0060e4ee9cc55deb82e15d569f93280031bf4
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-05-18 10:09:20 +0200

    renamed scripts

--- global.gd
+++ global.gd
@@ -7,6 +7,12 @@ const ROOM_TYPE_LECTURE_HALL = 3
 const ROOM_TYPE_OFFICE = 4
 const ROOM_TYPE_BATHROOM = 5
 
+# The direction of the room the door is connected with
+const DIRECTION_UP: int = 1
+const DIRECTION_DOWN: int = -1
+const DIRECTION_LEFT: int = 2
+const DIRECTION_RIGHT: int = -2
+
 var main_scene
 
 var current_room
