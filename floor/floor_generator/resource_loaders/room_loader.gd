1ec99a2718a4da0990990fffe668c445904bafa5
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-11-07 18:38:39 +0100

    Added classrooms, labs and bathrooms to room loader

--- floor/floor_generator/resource_loaders/room_loader.gd
+++ floor/floor_generator/resource_loaders/room_loader.gd
@@ -10,6 +10,20 @@ var inner_hallway_up_loader: ResourcePreloader
 var inner_hallway_left_loader: ResourcePreloader
 var inner_hallway_right_loader: ResourcePreloader
 
+var bathroom_up_loader: ResourcePreloader
+var bathroom_down_loader: ResourcePreloader
+var bathroom_right_loader: ResourcePreloader
+var bathroom_left_loader: ResourcePreloader
+
+var lab_up_loader: ResourcePreloader
+var lab_down_loader: ResourcePreloader
+var lab_right_loader: ResourcePreloader
+var lab_left_loader: ResourcePreloader
+
+var standard_classroom_up_loader: ResourcePreloader
+var standard_classroom_down_loader: ResourcePreloader
+var standard_classroom_right_loader: ResourcePreloader
+var standard_classroom_left_loader: ResourcePreloader
 
 func _ready() -> void:
 	hallway_up_loader = ResourcePreloader.new()
@@ -22,40 +36,94 @@ func _ready() -> void:
 	inner_hallway_left_loader = ResourcePreloader.new()
 	inner_hallway_right_loader = ResourcePreloader.new()
 	
+	bathroom_up_loader = ResourcePreloader.new()
+	bathroom_down_loader = ResourcePreloader.new()
+	bathroom_left_loader = ResourcePreloader.new()
+	bathroom_right_loader = ResourcePreloader.new()
+	
+	lab_up_loader = ResourcePreloader.new()
+	lab_down_loader = ResourcePreloader.new()
+	lab_left_loader = ResourcePreloader.new()
+	lab_right_loader = ResourcePreloader.new()
+
+	standard_classroom_up_loader = ResourcePreloader.new()
+	standard_classroom_down_loader = ResourcePreloader.new()
+	standard_classroom_left_loader = ResourcePreloader.new()
+	standard_classroom_right_loader = ResourcePreloader.new()
 	
 func load_resources() -> void:
+	
+	# HALLWAYS ROOM LOADERS INIT
 	for i in range(0, Global.HALLWAY_ROOM_UP_COUNTER):
 		hallway_up_loader.add_resource(str(i), load(Global.HALLWAY_UP_FOLDER_PATH + str(i) + "_up.tscn"))
-	print(hallway_up_loader.name + " " + str(hallway_up_loader.get_resource_list().size()))
 
 	for i in range(0, Global.HALLWAY_ROOM_DOWN_COUNTER):
 		hallway_down_loader.add_resource(str(i), load(Global.HALLWAY_DOWN_FOLDER_PATH + str(i) + "_down.tscn"))
-	print(hallway_down_loader.name + " " + str(hallway_down_loader.get_resource_list().size()))
-
 
 	for i in range(0, Global.HALLWAY_ROOM_LEFT_COUNTER):
 		hallway_left_loader.add_resource(str(i), load(Global.HALLWAY_LEFT_FOLDER_PATH + str(i) + "_left.tscn"))
-	print(hallway_left_loader.name + " " + str(hallway_left_loader.get_resource_list().size()))
 	
 	for i in range(0, Global.HALLWAY_ROOM_RIGHT_COUNTER):
 		hallway_right_loader.add_resource(str(i), load(Global.HALLWAY_RIGHT_FOLDER_PATH + str(i) + "_right.tscn"))
-	print(hallway_right_loader.name + " " + str(hallway_right_loader.get_resource_list().size()))
-	
+
+
+	# INNER HALLWAYS ROOM LOADERS
 	for i in range(0, Global.INNER_HALLWAY_ROOM_UP_COUNTER):
 		inner_hallway_up_loader.add_resource(str(i), load(Global.INNER_HALLWAY_UP_FOLDER_PATH + str(i) + "_up.tscn"))
-	print(inner_hallway_up_loader.name + " " + str(inner_hallway_up_loader.get_resource_list().size()))
 	
 	for i in range(0, Global.INNER_HALLWAY_ROOM_DOWN_COUNTER):
 		inner_hallway_down_loader.add_resource(str(i), load(Global.INNER_HALLWAY_DOWN_FOLDER_PATH + str(i) + "_down.tscn"))
-	print(inner_hallway_down_loader.name + " " + str(inner_hallway_down_loader.get_resource_list().size()))
 
 	for i in range(0, Global.INNER_HALLWAY_ROOM_RIGHT_COUNTER):
 		inner_hallway_right_loader.add_resource(str(i), load(Global.INNER_HALLWAY_RIGHT_FOLDER_PATH + str(i) + "_right.tscn"))
-	print(inner_hallway_right_loader.name + " " + str(inner_hallway_right_loader.get_resource_list().size()))
 	
 	for i in range(0, Global.INNER_HALLWAY_ROOM_LEFT_COUNTER):
 		inner_hallway_left_loader.add_resource(str(i), load(Global.INNER_HALLWAY_LEFT_FOLDER_PATH + str(i) + "_left.tscn"))
-	print(inner_hallway_left_loader.name + " " + str(inner_hallway_left_loader.get_resource_list().size()))
+
+
+	# BATHROOMS ROOM LOADERS
+	for i in range(0, Global.BATHROOM_ROOM_UP_COUNTER):
+		bathroom_up_loader.add_resource(str(i), load(Global.BATHROOM_UP_FOLDER_PATH + str(i) + "_up.tscn"))
+	
+	for i in range(0, Global.BATHROOM_ROOM_DOWN_COUNTER):
+		bathroom_down_loader.add_resource(str(i), load(Global.BATHROOM_DOWN_FOLDER_PATH + str(i) + "_down.tscn"))
+	
+	for i in range(0, Global.BATHROOM_ROOM_RIGHT_COUNTER):
+		bathroom_right_loader.add_resource(str(i), load(Global.BATHROOM_RIGHT_FOLDER_PATH + str(i) + "_right.tscn"))
+	
+	for i in range(0, Global.BATHROOM_ROOM_LEFT_COUNTER):
+		bathroom_left_loader.add_resource(str(i), load(Global.BATHROOM_LEFT_FOLDER_PATH + str(i) + "_left.tscn"))
+
+
+	# LAB ROOM LOADERS
+	for i in range(0, Global.LAB_ROOM_UP_COUNTER):
+		lab_up_loader.add_resource(str(i), load(Global.LAB_UP_FOLDER_PATH + str(i) + "_up.tscn"))
+	
+	for i in range(0, Global.LAB_ROOM_DOWN_COUNTER):
+		lab_down_loader.add_resource(str(i), load(Global.LAB_DOWN_FOLDER_PATH + str(i) + "_down.tscn"))
+	
+	for i in range(0, Global.LAB_ROOM_RIGHT_COUNTER):
+		lab_right_loader.add_resource(str(i), load(Global.LAB_RIGHT_FOLDER_PATH + str(i) + "_right.tscn"))
+	
+	for i in range(0, Global.LAB_ROOM_LEFT_COUNTER):
+		lab_left_loader.add_resource(str(i), load(Global.LAB_LEFT_FOLDER_PATH + str(i) + "_left.tscn"))
+
+
+	# LAB ROOM LOADERS
+	for i in range(0, Global.STANDARD_CLASSROOM_ROOM_UP_COUNTER):
+		standard_classroom_up_loader.add_resource(str(i), load(Global.STANDARD_CLASSROOM_UP_FOLDER_PATH + str(i) + "_up.tscn"))
+	
+	for i in range(0, Global.STANDARD_CLASSROOM_ROOM_DOWN_COUNTER):
+		standard_classroom_down_loader.add_resource(str(i), load(Global.STANDARD_CLASSROOM_DOWN_FOLDER_PATH + str(i) + "_down.tscn"))
+	
+	for i in range(0, Global.STANDARD_CLASSROOM_ROOM_RIGHT_COUNTER):
+		standard_classroom_right_loader.add_resource(str(i), load(Global.STANDARD_CLASSROOM_RIGHT_FOLDER_PATH + str(i) + "_right.tscn"))
+	
+	for i in range(0, Global.STANDARD_CLASSROOM_ROOM_LEFT_COUNTER):
+		standard_classroom_left_loader.add_resource(str(i), load(Global.STANDARD_CLASSROOM_LEFT_FOLDER_PATH + str(i) + "_left.tscn"))
+
+
+
 
 func get_random_room(type: int, direction: int) -> Room:
 	# Determine the appropriate resource loader based on the room type and direction
@@ -71,6 +139,7 @@ func get_random_room(type: int, direction: int) -> Room:
 					loader = hallway_left_loader
 				Global.DIRECTION_RIGHT:
 					loader = hallway_right_loader
+					
 		Global.ROOM_TYPE_INNER_HALLWAY:
 			match direction:
 				Global.DIRECTION_UP:
@@ -81,6 +150,46 @@ func get_random_room(type: int, direction: int) -> Room:
 					loader = inner_hallway_left_loader
 				Global.DIRECTION_RIGHT:
 					loader = inner_hallway_right_loader
+		
+		Global.ROOM_TYPE_BATHROOM:
+			match direction:
+				Global.DIRECTION_UP:
+					loader = bathroom_up_loader
+				Global.DIRECTION_DOWN:
+					loader = bathroom_down_loader
+				Global.DIRECTION_LEFT:
+					loader = bathroom_left_loader
+				Global.DIRECTION_RIGHT:
+					loader = bathroom_right_loader
+		
+		Global.ROOM_TYPE_CLASSROOM:
+			var random_sub_room_type = randi() % 2
+			
+			match direction:
+				Global.DIRECTION_UP:
+					match random_sub_room_type:
+						0:
+							loader = lab_up_loader
+						1:
+							loader = standard_classroom_up_loader
+				Global.DIRECTION_DOWN:
+					match random_sub_room_type:
+						0:
+							loader = lab_down_loader
+						1:
+							loader = standard_classroom_down_loader
+				Global.DIRECTION_LEFT:
+					match random_sub_room_type:
+						0:
+							loader = lab_left_loader
+						1:
+							loader = standard_classroom_left_loader
+				Global.DIRECTION_RIGHT:
+					match random_sub_room_type:
+						0:
+							loader = lab_right_loader
+						1:
+							loader = standard_classroom_right_loader
 		_:
 			pass
 
