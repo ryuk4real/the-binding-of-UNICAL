949881c7f8dd1ccde1a1879e80a123e8b8ceff07
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2024-11-17 17:13:44 +0100

    Added myself as a player

--- entities/projectile/projectile.gd
+++ entities/projectile/projectile.gd
@@ -5,35 +5,30 @@ extends Entity
 @export var damage: int = 10
 var direction: Vector2
 var range: float = 3
+
 const RANGE_PENALTY: int = 2
 
 func _ready() -> void:
 	SignalBus.player_damage_changed.connect(_on_player_damage_changed)
 	animated_sprite_2d.play("BASIC")
 
-func _process(delta: float) -> void:
-	if not enabled:
-		return
-		
-	position += direction * speed * delta
-	range -= delta * RANGE_PENALTY
+func _process(_delta: float) -> void:
+	position += direction * speed * _delta
+	range -= _delta * RANGE_PENALTY
 	
-	collision = move_and_collide(velocity * delta)
+	collision = move_and_collide(velocity * _delta)
 	
 	if collision:
-		death_animation_start()
+		_death_animation_start()
+		velocity = velocity.slide(collision.get_normal())
 	
 	if range <= 0:
-		death_animation_start()
+		_death_animation_start()
+		queue_free()
 
 func _on_player_damage_changed() -> void:
 	damage = Global.player.damage
 
-func death_animation_start() -> void:
-	if not enabled:
-		return
-		
-	enabled = false
+func _death_animation_start() -> void:
 	animated_sprite_2d.play("DEATH")
 	await animated_sprite_2d.animation_finished
-	queue_free()
