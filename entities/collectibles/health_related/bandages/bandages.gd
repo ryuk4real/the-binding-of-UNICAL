0fe57586eb01fe359e224cd353a9cb574b02edee
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-11-20 12:39:20 +0100

    Inverted bandages and medikit effect

--- entities/collectibles/health_related/bandages/bandages.gd
+++ entities/collectibles/health_related/bandages/bandages.gd
@@ -1,9 +1,11 @@
 class_name Bandages
 extends Collectible
 
+@export var heal_amount: float = 25.0
+
 func _ready() -> void:
 	super._ready()
 	collectible_type = CollectibleType.HEALTH
 
 func apply_effect(collector: Player) -> void:
-	collector.heal(collector.max_hp)
+	collector.heal(heal_amount)
--- entities/collectibles/health_related/medikit/medikit.gd
+++ entities/collectibles/health_related/medikit/medikit.gd
@@ -1,11 +1,9 @@
 class_name Medikit
 extends Collectible
 
-@export var heal_amount: float = 100.0
-
 func _ready() -> void:
 	super._ready()
 	collectible_type = CollectibleType.HEALTH
 
 func apply_effect(collector: Player) -> void:
-	collector.heal(heal_amount)
+	collector.heal(collector.max_hp)
